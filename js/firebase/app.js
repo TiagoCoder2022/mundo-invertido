b88aefbce35f4f7c4429c013c7174708a4c4ca9c
TiagoCoder2022 <tiagonewalves@hotmail.com>
2023-04-08 01:59:57 -0300

    first commit

---
+++ js/firebase/app.js
@@ -0,0 +1,19 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCuTA0m3QFUhzdjcxKz7uT6PvFMXO8HsrI",
+  authDomain: "mundo-invertido-5c16d.firebaseapp.com",
+  projectId: "mundo-invertido-5c16d",
+  storageBucket: "mundo-invertido-5c16d.appspot.com",
+  messagingSenderId: "662628280150",
+  appId: "1:662628280150:web:4a68196ba689f3733b98f7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app
